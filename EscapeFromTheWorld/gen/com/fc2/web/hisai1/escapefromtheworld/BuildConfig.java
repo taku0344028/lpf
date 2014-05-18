e013a2741bdb717f8fdb7d88ce5ed63d8ff9d71f
Takuya Okubo <taku0344028@gmail.com>
2014-05-18 09:13:53 +0900

    test

---
+++ EscapeFromTheWorld/gen/com/fc2/web/hisai1/escapefromtheworld/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.fc2.web.hisai1.escapefromtheworld;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
